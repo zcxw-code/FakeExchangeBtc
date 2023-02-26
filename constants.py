aeee5acb97cadb921d4ecb84ca368edeb57f660f
zcxw-code <zcxw.code@hotmail.com>
2023-02-26 21:30:01 +0300

    first commit

---
+++ constants.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+# !usr/bin/env python
+
+token = "1234:1qdqdkqdkqdkqdkqdkq"
+BOT_USERNAME = 'TEST'
+BITCOIN_ADDERS = '1234s5d67s8dd9s0sd'
+BitcomatHelpBot = 'HELP'
+PHONE_QIWI = '123456789'
